c5fe4fadcd6d9426589c82e0df6015b1051f1772
campcoral <kl.kiruxa2006@gmail.com>
2024-09-10 09:29:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
-let passInput = document.getElementsByName("PasswordInput")[0];
-let repassInput = document.getElementsByName("RePasswordInput")[0];
-let submitButton = document.getElementsByName("SubmitButton")[0];
 
-submitButton.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    if (passInput.value !== repassInput.value) {
-        alert("Passwords do not match");
-    } else {
-        alert("Registration successful");
-    }
-});
